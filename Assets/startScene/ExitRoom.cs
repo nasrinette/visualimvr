e065111ac3631b21c4762273fef03bf01b6ed52e
ksenichault <kimia.senichault@gmail.com>
2026-01-23 16:18:39 +0100

    added street scene and start scene

---
+++ Assets/startScene/ExitRoom.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitRoom : MonoBehaviour
+{
+
+    public GameObject supermarket;
+    public GameObject street;
+    public GameObject classroom;
+
+    public GameObject supermarketFrame;
+    public GameObject streetFrame;
+    public GameObject classroomFrame;
+
+
+    public GameObject baseRoom;
+
+    // Start is called before the first frame update
+ 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            Debug.Log("exit");
+            supermarket.SetActive(false);
+            street.SetActive(false);
+            classroom.SetActive(false);
+
+
+            baseRoom.SetActive(true);
+
+            supermarketFrame.SetActive(true);
+            streetFrame.SetActive(true);
+            classroomFrame.SetActive(true);
+
+        }
+    }
+   
+}
